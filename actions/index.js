460738acd81926aeb3abc2de140807532e0fdf74
Aqueum <martin.currie@aqueum.com>
2018-04-25 15:54:17 +0100

    part way, but first commit

---
+++ actions/index.js
@@ -0,0 +1,18 @@
+// action constants
+export const RECEIVE_ENTRIES = 'RECEIVE_ENTRIES';
+export const ADD_ENTRY = 'ADD_ENTRY';
+
+// action creators
+export function receiveEntries(entries) {
+  return {
+    type: RECEIVE_ENTRIES,
+    entries
+  };
+}
+
+export function addEntry(entry) {
+  return {
+    type: ADD_ENTRY,
+    entry
+  };
+}
