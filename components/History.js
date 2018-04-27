c3b008a4557211db599232b7f2d38c36775d3e0f
Aqueum <martin.currie@aqueum.com>
2018-04-27 18:03:52 +0100

    feat: Add history placeholder

---
+++ components/History.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class History extends Component {
+  render() {
+    return (
+      <View>
+        <Text>History</Text>
+      </View>
+    );
+  }
+}
