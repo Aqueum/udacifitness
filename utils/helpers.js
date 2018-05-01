78fa21cc7efe07d654345843c3c94e5575e739a9
Aqueum <martin.currie@aqueum.com>
2018-05-01 12:09:14 +0100

    fix: Typo

--- utils/helpers.js
+++ utils/helpers.js
@@ -151,7 +151,7 @@ export function getDailyReminderValue() {
 
 export function clearLocalNotification() {
   return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
-    Notifications.cancelAllScheduledNotificationAsync()
+    Notifications.cancelAllScheduledNotificationsAsync()
   );
 }
 
@@ -178,7 +178,7 @@ export function setLocalNotification() {
       if (data === null) {
         Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
           if (status === 'granted') {
-            Notifications.cancelAllScheduledNotificationAsync();
+            Notifications.cancelAllScheduledNotificationsAsync();
 
             let tomorrow = new Date();
             tomorrow.setDate(tomorrow.getDate() + 1);
